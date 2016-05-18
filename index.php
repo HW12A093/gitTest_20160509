e64a15d6d8fc0b9a4ec4df1df6eb9eee83f16d97
Hosomi <hosomi@ipride.co.jp>
2016-05-18 17:18:34 +0900

    AtomでPHPのソース書いたら「'」だとかが自動補完されてソースが変になってたのを修正

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
   print('<p>Hello!World!'</p>);
   print('<p>commitTest'</p>);
-  print('<p>New branch test! Is it test branch?</p>'');
-  print('<p>testブランチの使い方がめちゃくちゃになってる。知らない間にマージしてるし。</p>'');
+  print('<p>New branch test! Is it test branch?</p>');
+  print('<p>testブランチの使い方がめちゃくちゃになってる。知らない間にマージしてるし。</p>);
 ?>
