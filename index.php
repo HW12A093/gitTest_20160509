e17a6e3f0e1accdf915cc085971c0ead19dcbf2d
Hosomi <hosomi@ipride.co.jp>
2016-05-18 17:26:41 +0900

    testブランチにちゃんと意図したとおりにコミットできてる。 あと日本語コメント書けるんやな～、意外。

--- index.php
+++ index.php
@@ -3,4 +3,5 @@
   print('<p>commitTest'</p>);
   print('<p>New branch test! Is it test branch?</p>');
   print('<p>testブランチの使い方がめちゃくちゃになってる。知らない間にマージしてるし。</p>);
+  print('<p>なんとなくbranchの使い方わかってきた Mergeがまだよく分からない 2016年5月18日17時25分</p>');
 ?>
