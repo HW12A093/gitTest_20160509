8eba1558e41f9f2b45d2fe9f4a75ed1ecd577d0e
Hosomi <hosomi@ipride.co.jp>
2016-05-18 17:52:25 +0900

    PullRequestの練習

--- index.php
+++ index.php
@@ -4,4 +4,5 @@
   print('<p>New branch test! Is it test branch?</p>');
   print('<p>testブランチの使い方がめちゃくちゃになってる。知らない間にマージしてるし。</p>);
   print('<p>なんとなくbranchの使い方わかってきた Mergeがまだよく分からない 2016年5月18日17時25分</p>');
+  print('<p>せっかくなのでPull Requestの練習をする 2016年5月18日17時51分</p>');
 ?>
