c1be1cae1f80df8e1b7d27e65b6e132ed310d437
unknown <hosomi@ipride.co.jp>
2016-05-19 14:48:54 +0900

    First commit!

--- index.php
+++ index.php
@@ -1,8 +1,3 @@
 <?php
-  print('<p>Hello!World!'</p>);
-  print('<p>commitTest'</p>);
-  print('<p>New branch test! Is it test branch?</p>');
-  print('<p>testブランチの使い方がめちゃくちゃになってる。知らない間にマージしてるし。</p>);
-  print('<p>なんとなくbranchの使い方わかってきた Mergeがまだよく分からない 2016年5月18日17時25分</p>');
-  print('<p>せっかくなのでPull Requestの練習をする 2016年5月18日17時51分</p>');
+  print('<p>Hello!World!</p>');
 ?>
