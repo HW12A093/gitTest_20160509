d40726c0230c8eabcd14379e18c3c25788d8d762
unknown <hosomi@ipride.co.jp>
2016-05-19 15:06:57 +0900

    Add Atom command list file.

---
+++ command.php
@@ -0,0 +1,29 @@
+<html>
+<head>
+<title>Atomエディタを使うための試し書きノート</title>
+</head>
+<body>
+<?php
+    print('Hello! World');
+
+    print('<p><b>ショートカットキー一覧: http://qiita.com/4cres/items/cb3356ea0de2835957f0b</b></p>');
+    print('<p><b>チートシート: http://qiita.com/ngmdesign/items/fa84b9d4be1e4b7a82b8</b></p>');
+
+    print('<p>Ctrl + 文字列クリック: カーソルを増やして一括入力 ESCでキャンセル</p>');
+    print('<p>Ctrl + L: 行選択</p>');
+    print('<p>Ctrl + Shift + K: 行削除</p>');
+    print('<p>Ctrl + ↑↓: 行ごと移動</p>');
+    print('<p>Ctrl + ] [: インデント増減</p>');
+    print('<p>Ctrl + Shift + D: 選択中の行を下に複製</p>');
+    print('<p>Ctrl + Enter: 空白行を追加</p>');
+    print('<p>Ctrl + Shift + Enter: 上に空白行を追加</p>');
+    print('<p>Ctrl + /: コメントアウト</p>');
+    print('<p>Ctrl + ,: 設定画面表示</p>');
+
+    print('<p><b>git-plus</b></p>');
+    print('<p>Ctrl + Shift + H: Gitの検索窓メニューを開く</p>');
+    print('<p>検索窓は曖昧検索が可能で、aaacと打てばAdd All And Commitが出てくる</p>');
+    print('<p>ESCで検索窓を閉じる</p>');
+?>
+</body>
+</html>
